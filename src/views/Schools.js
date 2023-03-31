0be14613cfd24e0755c58ce683187195ba78a572
sanderpalk <sanderpalk0@gmail.com>
2023-03-31 16:32:42 +0300

    Final project

---
+++ src/views/Schools.js
@@ -0,0 +1,16 @@
+function Schools() {
+    return (
+        <div>
+            <div className="section-experience-content-item">
+                <h2>Tartu Vocational College</h2>
+                <p className="time-frame">January - December 2022</p>
+            </div>
+            <div className="section-experience-content-item">
+                <h2>Tõrva Gymnasium</h2>
+                <p className="time-frame">January - December 2022</p>
+            </div>
+        </div>
+    )
+}
+
+export default Schools
---
+++ src/views/Work.js
@@ -0,0 +1,39 @@
+function Work() {
+    return (
+        <div className="section-experience-content-wrapper">
+            <div className="section-experience-content-item">
+                <h2>Internship @ Codelive</h2>
+                <p className="time-frame">January - December 2022</p>
+                <ul>
+                    <li>Imported users, orders and products from client's database to Wordpress database with MariaDb
+                        queries. The information amount was 300 000+ orders, 20 000+ users and over 2500 products.
+                    </li>
+                    <li>Built Wordpress plugin to sync client's database with Worpdress database, so the warehouse
+                        logistics logic could be brought over and learned by employees step by step. This included
+                        users, products information and stock, order information and statuses.
+                    </li>
+                </ul>
+            </div>
+            <div className="section-experience-content-item">
+                <h2>Payed employment @ Codelive</h2>
+                <p className="time-frame">January - December 2022</p>
+                <ul>
+                    <li>Worked with considerable data models for investment company, mostly make big queries and reports
+                        faster and more efficient.
+                    </li>
+                    <li>Front-end implementation to ACF plugin on Wordpress pages.</li>
+                </ul>
+            </div>
+            <div className="section-experience-content-item">
+                <h2>Internship @ Codelive</h2>
+                <p className="time-frame">January - December 2022</p>
+                <ul>
+                    <li>Added back-end logic to start-up company.</li>
+                    <li>Developed Wordpress pages with ACF.</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Work
